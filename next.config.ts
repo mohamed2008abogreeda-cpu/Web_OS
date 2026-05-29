c846f6db1847984149440dab2948703ff0a00ff6
mohamed2008abogreeda-cpu <mohamed2008abogreeda-cpu@users.noreply.github.com>
2026-05-30 02:26:41 +0300

    chore: allow local developer network IP in allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 initOpenNextCloudflareForDev();
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["webos.foggystorm.dpdns.org"],
+  allowedDevOrigins: ["webos.foggystorm.dpdns.org", "192.168.1.6"],
 };
 
 export default nextConfig;
